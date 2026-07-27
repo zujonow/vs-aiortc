f91baa74eb7c35bd32df829bc886b6ab939257c3
Deep Bhupatkar <bhupatkardeep@gmail.com>
2026-07-27 12:57:43 +0530

    version up

--- src/vsaiortc/__init__.py
+++ src/vsaiortc/__init__.py
@@ -50,7 +50,7 @@ from .stats import (
     RTCTransportStats,
 )
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 # Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
